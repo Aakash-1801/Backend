34676f50c7a233357ae453cce34d43d193fc9167
osamabenlade <randomb921@gmail.com>
2025-06-12 12:37:18 +0530

    check commit

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
